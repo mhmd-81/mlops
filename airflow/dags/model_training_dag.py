5dc81af12e79e88a38415e7b99c1f1444dd478d4
amin-alem <mohammad81.ma09@gmail.com>
2026-08-12 01:39:15 +0330

    added airflow, its docker, requirements.txt

---
+++ airflow/dags/model_training_dag.py
@@ -0,0 +1,24 @@
+import pendulum
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+
+
+with DAG(
+    dag_id = 'model_training',
+    start_date = pendulum.datetime(
+        2026, 8, 11,
+        tz= 'Asia/Tehran'
+    ),
+    schedule="*/15 * * * *",
+    catchup= False,
+    max_active_runs = 1,
+    tags = ['ml','training']
+
+) as dag:
+    run_model = BashOperator(
+        task_id = 'run_model',
+        bash_command="python /opt/airflow/src/model_train.py",
+        retries = 2,
+        retry_delay = pendulum.duration(minutes=5),
+    )
