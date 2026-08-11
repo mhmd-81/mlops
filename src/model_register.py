215aec2ff8b8b32c919db08b2df3ea1e8e782e00
amin-alem <mohammad81.ma09@gmail.com>
2026-08-11 22:07:53 +0330

    deleted windows-side req file edit model registery add data in seperated file

--- src/model_register.py
+++ src/model_register.py
@@ -11,27 +11,35 @@ client = MlflowClient()
 model_name = os.getenv('MODEL_NAME')
 
 
+def candidate_model():
+    version = client.search_model_versions(
+        f"name='{model_name}'"
+    )
 
-version = client.search_model_versions(
-    f"name='{model_name}'"
-)
-
-latest_version = max(
-    version,
-    key = lambda x: int(x.version)
-)
-
-
-
-client.set_registered_model_alias(
-    model_name,
-    'candidate',
-    latest_version.version
-)
+    latest_version = max(
+        version,
+        key = lambda x: int(x.version)
+    )
 
 
-print(
-    f"Model version {latest_version.version} "
-    f"promoted to candidate"
-)
 
+    client.set_registered_model_alias(
+        model_name,
+        'candidate',
+        latest_version.version
+    )
+
+
+    print(
+        f"Model version {latest_version.version} "
+        f"promoted to candidate"
+    )
+
+def eval_model():
+    candidate = client.get_model_version_by_alias(
+        model_name,
+        'candidate'
+    )
+    model_uri = f"models/{model_name}@candidate"
+    model = mlflow.sklearn.load_model(model_uri)
+    predictions = model.predict(X_test)
