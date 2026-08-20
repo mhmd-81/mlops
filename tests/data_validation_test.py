894e468ca499516f73c041b040bdb37c5800ac01
amin-alem <mohammad81.ma09@gmail.com>
2026-08-20 10:05:39 +0330

    added dockerfile.tests

--- tests/data_validation_test.py
+++ tests/data_validation_test.py
@@ -7,11 +7,12 @@ from pathlib import Path
 data_path = Path(__file__).parent.parent/"data"/"iris.csv"
 
 expected_columns = {
-    "sepal_length": "float64",
-    "sepal_width": "float64",
-    "petal_length": "float64",
-    "petal_width": "float64",
-    "species": "object",
+    "sepal length (cm)": "float64",
+    "sepal width (cm)": "float64",
+    "petal length (cm)": "float64",
+    "petal width (cm)": "float64",
+    "target": "int64",
+    "target_name": "object",
 }
 
 valid_outputs = {"setosa", "versicolor", "virginica"}
@@ -28,11 +29,18 @@ numeric_ranges = {
 }
 
 @pytest.fixture(scope="module")
-def data():
+def data_existance():
     assert data_path.exists(), f"data file not found at {data_path}"
     return pd.read_csv(data_path)
-def test_data_not_empty(data):
-    assert not data.empty
+def test_data_not_empty(data_existance):
+    assert not data_existance.empty
 
 
 
+def test_expected_columns_check(data_existance):
+    missing = set(expected_columns) - set(data_existance.columns)
+    assert not missing, f'missing expected columns {missing}'
+
+def test_unexpected_columns(data_existance):
+    unexpected = set(data_existance.columns) - set(expected_columns)
+    assert not unexpected, 'unexpected columns found'
